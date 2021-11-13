44d4d0ce6df3f7da66314bf5b0fbac681431ee07
Dell <abdoo.chbrik@gmail.com>
2021-11-13 17:00:53 +0100

    make our api gateway be able to create dynamically the servcie route using the eureka server

--- demo/src/main/java/com/apigateway/demo/ApigatewayApplication.java
+++ demo/src/main/java/com/apigateway/demo/ApigatewayApplication.java
@@ -2,6 +2,11 @@ package com.apigateway.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApigatewayApplication {
@@ -9,5 +14,18 @@ public class ApigatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
 	}
+
+	/*
+	 * Dynamic routing locator using the eureka server
+	 * */
+	@Bean
+	public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(ReactiveDiscoveryClient discoveryClient, DiscoveryLocatorProperties properties) {
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+	}
+
+
+	/*
+	 * static routing to the other services
+	 * */
 
 }
